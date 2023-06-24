d05b28d05d8411eb455f4291129ce934706746c6
Kaylat30 <kayonndoabdulatif.com>
2023-06-24 15:25:57 +0300

    Added real dots on the dice

--- src/Die.jsx
+++ src/Die.jsx
@@ -3,9 +3,19 @@ export default function Die(props) {
     const styles = {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
+    const renderDiceDots = () => {
+        const dots = [];
+    
+        for (let i = 0; i < props.value; i++) {
+          dots.push(<div key={i} className="die-num">.</div>);
+        }
+    
+        return dots;
+      };
     return (
         <div className="die-face" onClick={props.hold} style={styles}>
-            <h2 className="die-num">{props.value}</h2>
+            {/* <h2 className="die-num">{props.value}</h2> */}
+            {renderDiceDots()}
         </div>
     )
 }
